2a7400633ffb773edd24034530cd8f159fa99ad6
hjjjong0811 <hjjjong0811@gmail.com>
2018-04-13 22:10:08 +0900

    디자인개선/탭변경/스크롤오류수정/소스코드정리

--- src/renderer/NavPanel.jsx
+++ src/renderer/NavPanel.jsx
@@ -1,31 +1,22 @@
 import React from "react";
-import {Popover, Overlay, Button, FormControl, Form,
-            Tabs, Tab, ListGroup} from "react-bootstrap";
 import style from "./css/NavPanel.css";
 import ListItem from "./ListItem";
 import {ipcRenderer} from "electron";
+import FileList from "./FileList";
 
 export default class NavPanel extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             //Data
-            newFileName: "",
             searchText: "",
 
             //UI
-            isOpenCreate: false,
             activeTab: 1
         };
 
         this.OnSearchChange = this.OnSearchChange.bind(this);
         this.OnSearchClick = this.OnSearchClick.bind(this);
-
-        this.CreateWin_Toggle = this.CreateWin_Toggle.bind(this);
-        this.OnFileNameChange = this.OnFileNameChange.bind(this);
-        this.OnCreateWin_OKClick = this.OnCreateWin_OKClick.bind(this);
-
-        this.onClickTab = this.onClickTab.bind(this);
     }
 
 
@@ -37,31 +28,6 @@ export default class NavPanel extends React.Component{
         e.preventDefault();
         console.log("OnSearchClick : "+this.state.searchText);
     }
-    // * CreateFile
-    CreateWin_Toggle(e){
-        this.setState({
-            isOpenCreate: !this.state.isOpenCreate,
-            target: e.target
-        });
-    }
-    OnFileNameChange(e){
-        this.setState({newFileName: e.target.value});
-    }
-    OnCreateWin_OKClick(e){
-        e.preventDefault();
-        //파일명규칙검사필요
-        //props.files에 동일파일명존재 검사도
-        ipcRenderer.send("RM_NEWFILE", this.state.newFileName + ".txt");
-        this.setState({
-            newFileName: "",
-            isOpenCreate: false
-        });
-    }
-
-    // * File Select
-    onClickFile(fileName){
-        ipcRenderer.send("RM_OPENFILE", fileName);
-    }
 
     // * Tab Click
     onClickTab(key){
@@ -70,18 +36,6 @@ export default class NavPanel extends React.Component{
     
 
     // * Render
-    renderFileList(){
-        return(
-            <ListGroup>
-                {this.props.files.map(f=>
-                    <ListItem
-                        txtInfo={f}
-                        selected={f.name === this.props.selectedFile}
-                        onClickItem={this.onClickFile.bind(this, f.name)}/>
-                    )}
-            </ListGroup>
-        );
-    }
     renderTabList(){
         return(
             <div>
@@ -126,28 +80,28 @@ export default class NavPanel extends React.Component{
                 </div>
                 <hr width="100%"/>
 
-                {/* FileList */}
+                {/* File Or Tag List */}
                 <div className={style.flexRemain}>
-                    <Tabs
-                        activeKey={this.state.activeTab}
-                        onSelect={this.onClickTab}
-                    >
-                        <Tab eventKey={1} title="FileList">
-                            <Button onClick={this.CreateWin_Toggle}>Create TextFile</Button>
-                            <Overlay
-                                show={this.state.isOpenCreate}
-                                target={this.state.target}
-                                placement="right">
-                                    {this.renderCreatePopup()}
-                            </Overlay>
-                            <div className={style.scrollBox}>
-                                {this.renderFileList()}
-                            </div>
-                        </Tab>
-                        <Tab eventKey={2} title="Tags">
-                            {this.renderTabList()}
-                        </Tab>
-                    </Tabs>
+                    {/* Tab Button.. */}
+                    <div>
+                        <span
+                            className={this.state.activeTab === 1? style.btnSimple_Selected : style.btnSimple}
+                            onClick={this.onClickTab.bind(this, 1)}>
+                                Files
+                        </span>
+                        <span
+                            className={this.state.activeTab === 2? style.btnSimple_Selected : style.btnSimple}
+                            onClick={this.onClickTab.bind(this, 2)}>
+                                Tags
+                        </span>
+                    </div>
+                    {/* Tab Contents */}
+                    <div class={this.state.activeTab === 1?style.tab_Active:style.tab_Disable}>
+                        <FileList files={this.props.files} selectedFile={this.props.selectedFile}/>
+                    </div>
+                    <div class={this.state.activeTab === 2?style.tab_Active:style.tab_Disable}>
+                        taglist
+                    </div>
                 </div>
             </div>
         );
